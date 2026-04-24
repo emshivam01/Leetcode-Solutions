55357b5b58d55dc0bdc34a676b6aac7e20c5b0f1
Shivam Sharma <emshivam01@gmail.com>
2026-04-24 12:18:20 +0530

    Initial commit

---
+++ Arrays/014_LongestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string ans;
+        for (int i = 0; i < strs[0].size(); i++)
+        {
+            for (int j = 1; j < strs.size(); j++)
+            {
+                if (i >= strs[j].size() || strs[j][i] != strs[0][i])
+                {
+                    return ans;
+                }
+            }
+            ans = ans + strs[0][i];
+        }
+        return ans;
+    }
+};
