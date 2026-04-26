5e224f1c15df88daedfcbc39a846d03cff5ac049
Shivam Sharma <emshivam01@gmail.com>
2026-04-26 12:16:06 +0530

    Solve Problem_27(Remove Element)

---
+++ Arrays/027_RemoveElement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int writeIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[writeIndex] = nums[i];
+                writeIndex++;
+            }
+        }
+        return writeIndex;
+    }
+};
